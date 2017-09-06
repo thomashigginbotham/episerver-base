66eb14824ba2af6494040faa6572ab7aa075572a
Thomas Higginbotham <thomas@thomashigginbotham.com>
2017-09-06 16:18:28 -0400

    Create start page

---
+++ EpiserverBase/Models/Pages/StartPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverBase.Models.Pages
+{
+	[ContentType(DisplayName = "StartPage", GUID = "5f5a391f-6b6a-4659-8e40-67a1860c2e52", Description = "")]
+	public class StartPage : PageData
+	{
+
+		[CultureSpecific]
+		[Display(
+			Name = "Main body",
+			Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+			GroupName = SystemTabNames.Content,
+			Order = 1)]
+		public virtual XhtmlString MainBody { get; set; }
+
+	}
+}
