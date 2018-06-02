a4567d1787c35176d15fd3ccdcf5a1091f4c80bc
Thomas Higginbotham <thomas@thomashigginbotham.com>
2018-06-01 23:25:22 -0400

    Add sizing options to images

--- EpiserverBase/Models/Media/ImageFile.cs
+++ EpiserverBase/Models/Media/ImageFile.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
+using EPiServer.Framework.Blobs;
 using EPiServer.Framework.DataAnnotations;
 
 namespace EpiserverBase.Models.Media
@@ -19,5 +20,26 @@ namespace EpiserverBase.Models.Media
             GroupName = SystemTabNames.Content,
             Order = 100)]
         public virtual string Description { get; set; }
+
+        /// <summary>
+        /// A small version of the image (accessible at "/path/to/image.jpg/Small").
+        /// </summary>
+        [ScaffoldColumn(false)]
+        [ImageDescriptor(Width = 640, Height = 480)]
+        public virtual Blob Small { get; set; }
+
+        /// <summary>
+        /// A medium version of the image (accessible at "/path/to/image.jpg/Medium").
+        /// </summary>
+        [ScaffoldColumn(false)]
+        [ImageDescriptor(Width = 800, Height = 600)]
+        public virtual Blob Medium { get; set; }
+
+        /// <summary>
+        /// A large version of the image (accessible at "/path/to/image.jpg/Large").
+        /// </summary>
+        [ScaffoldColumn(false)]
+        [ImageDescriptor(Width = 1200, Height = 900)]
+        public virtual Blob Large { get; set; }
     }
 }
